a641c636a2b189651e0c2a77c4f26fc453fe435c
Shadow <taltaro2@gmail.com>
2020-02-07 15:38:49 -0500

    sent

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const url = 'https://kitsu.io/api/edge'
 // let url2 = url + Input.value "/"
+
+fetch(url)
+        .then(res => {
+            console.log('success', res) // if it works well gt this 
+    return res.json()
+        })
+        .then(res => {
+            console.log(res)
+        })   
+        .catch(err => {
+            console.log('something went wrong', err) // if it doest work well get this
+        })
+        
