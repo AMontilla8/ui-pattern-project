22e7b9e7fc36f0aa04e08c9c3cf142e94c323e35
Shadow <taltaro2@gmail.com>
2020-02-09 23:46:32 -0500

    status update?

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const url = 'https://kitsu.io/api/edge/'
+const url = 'https://kitsu.io/api/edge/anime'
 // let url2 = url + Input.value "/"
+let tittle = document.querySelectorAll('.animeTittle')
+let images = document.querySelectorAll('animeImage')
 
 fetch(url, {
-    headers: { 
-        'application/vnd.api+json` : 'Accept: application/vnd.api+json'
-}
-        .then(res => {
-            console.log('success', res) // if it works well gt this 
-    return res.json()
-        })
-        .then(res => {
-            console.log(res)
-        })   
-        .catch(err => {
-            console.log('something went wrong', err) // if it doest work well get this
-        })
-
-        
+    headers :{ 
+        'Content-Type': 'application/vnd.api+json', 
+        'Accept': 'application/vnd.api+json'
+    }
+})
+    .then(res => {
+        console.log('success', res) // if it works well gt this 
+        return res.json()
+    })
+    .then(res => {
+        console.log(res)
+    })   
+    .catch(err => {
+        console.log('something went wrong', err) // if it doest work well get this
+    })
+    
